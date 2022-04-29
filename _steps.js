c66020f60c4f7021926a2609e469ff0c5c9f9fa3
RatonRoy <royraton18@gmail.com>
2022-04-30 03:46:29 +0600

    setup files

---
+++ _steps.js
@@ -0,0 +1,6 @@
+/* *******************************
+----------------------------
+Requirements of installation which npm  packets are needs to run server side with mongo db. 
+___________________________________________
+1. 
+*******************************/
